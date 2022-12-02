b02babcd8f95226fafacd7280f1bcab552ae6a62
Vairelles1409 <vairellesfonkou@gmail.com>
2022-12-02 09:51:47 +0100

    Vue+Ges_BDD

---
+++ Code/View/View + Ges_BDD/Form2.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace GesRestaurant.View
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Ajouter = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Nom = new System.Windows.Forms.Label();
+            this.Prix = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Ajouter
+            // 
+            this.Ajouter.Location = new System.Drawing.Point(455, 268);
+            this.Ajouter.Name = "Ajouter";
+            this.Ajouter.Size = new System.Drawing.Size(75, 23);
+            this.Ajouter.TabIndex = 0;
+            this.Ajouter.Text = "button1";
+            this.Ajouter.UseVisualStyleBackColor = true;
+            this.Ajouter.Click += new System.EventHandler(this.Ajouter_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(332, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Nom
+            // 
+            this.Nom.AutoSize = true;
+            this.Nom.Location = new System.Drawing.Point(159, 48);
+            this.Nom.Name = "Nom";
+            this.Nom.Size = new System.Drawing.Size(35, 13);
+            this.Nom.TabIndex = 2;
+            this.Nom.Text = "label1";
+            // 
+            // Prix
+            // 
+            this.Prix.AutoSize = true;
+            this.Prix.Location = new System.Drawing.Point(159, 117);
+            this.Prix.Name = "Prix";
+            this.Prix.Size = new System.Drawing.Size(35, 13);
+            this.Prix.TabIndex = 3;
+            this.Prix.Text = "label2";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(332, 114);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Prix);
+            this.Controls.Add(this.Nom);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Ajouter);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Ajouter;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label Nom;
+        private System.Windows.Forms.Label Prix;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
---
+++ Code/View/View + Ges_BDD/Form2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace GesRestaurant.View
+{
+    public partial class Form2 : Form
+    {
+        SqlConnection cnx;
+        public Form2()
+        {
+            InitializeComponent();
+            cnx = new SqlConnection(@"Data Source=PCVairelles;Initial Catalog=GesRestauration;Integrated Security=True");
+        }
+
+        private void Ajouter_Click(object sender, EventArgs e)
+        {
+            cnx.Open();
+            String sql = "insert into Recette(Nom, Prix)values("+ textBox1.Text + ",'" + textBox2.Text + ")";
+            SqlCommand cmd = new SqlCommand(sql, cnx);
+            cmd.ExecuteNonQuery();
+            cnx.Close();
+
+        }
+    }
+}
